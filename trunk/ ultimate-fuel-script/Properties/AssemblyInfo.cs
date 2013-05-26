7be212852ad0f94f9ce1cd40f5c283ece1aa67e8
prodrigues1990@gmail.com <prodrigues1990@gmail.com@54489e81-a356-c73c-d07a-b86c62fa8e83>
2013-05-26 21:46:39 +0000

    Ported to VS 2010

---
+++ trunk/ ultimate-fuel-script/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UltimateFuelScript")]
+[assembly: AssemblyDescription("GTA IV Mod")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pedro Rodrigues <prodrigues1990@gmail.com>")]
+[assembly: AssemblyProduct("UltimateFuelScript")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d6a65da-aba6-4a4a-bc2a-25628c892dab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
